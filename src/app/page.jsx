f89986fa6eaf42ff1edb403494c1c83ff7798b36
Aditya Vaish <adityavaish846@gmail.com>
2025-05-26 14:15:07 -0700

    necessary packages installed

---
+++ src/app/page.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="min-h-screen grid grid-rows-[auto_1fr_auto] display-flex flex-col items-center justify-center text-center">
+      <h1>Mudex Music</h1>
+      <p>Elevate the Underrated</p>
+    </div>
+  );
+}
